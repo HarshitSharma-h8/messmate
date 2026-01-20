38566c15d6c0d291fa7da7b10457a8b7d3d7c3a1
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2026-01-20 19:45:40 +0530

    Add nodemon configuration and enhance CORS middleware settings

--- src/app.js
+++ src/app.js
@@ -12,7 +12,14 @@ import adminRoutes from "./routes/Admin.routes.js";
 const app = express();
 
 /* ---------- Global Middlewares ---------- */
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 
